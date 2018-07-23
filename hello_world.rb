96f52184e1cc73c0021e08cc03c0e309bb9ab8a4
nicklennon <nicklennon.53@gmail.com>
2018-07-23 19:43:30 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,7 +1,7 @@
 # Take in the user's name as input and output a personalized greeting 
-puts "What is your name?"
-name = gets
 
+name = gets
+puts "What is your name?"
 puts name
 
 # name = "Arella"
